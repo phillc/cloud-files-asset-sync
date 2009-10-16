392c018a1eb64a5d1246b8e410d617502635a61f
phillc <pccampbell@patrickdavis.com>
2009-10-16 16:18:34 -0400

    modifications and refactors so I can try this new idea out

--- lib/cloudfile_asset.rb
+++ lib/cloudfile_asset.rb
@@ -3,15 +3,20 @@ require 'yaml'
 
 module CloudfileAsset
   class << self
-    def sync_public(loud=false, wait=nil)
+    def get_local_public_path
+      "#{RAILS_ROOT}/public/"
+    end
+    
+    def get_container
       cloud_config = YAML.load_file("#{RAILS_ROOT}/config/cloud_files.yml")['production'].symbolize_keys
       
       cf = CloudFiles::Connection.new(cloud_config[:username], cloud_config[:api_key])
       container = cf.container(cloud_config[:assets_container])
-          
-      @dir_path = "#{RAILS_ROOT}/public/"
-
-      files = Dir[[@dir_path, '**/*.*'].join].reject do |file|
+      return container
+    end
+    
+    def get_local_public_files
+      files = Dir[[self.get_local_public_path, '**/*.*'].join].reject do |file|
         extension = file.split('.').last
         case extension
         when 'cgi', 'fcgi', 'rb', 'sass'
@@ -20,6 +25,12 @@ module CloudfileAsset
           false
         end
       end
+      return files
+    end
+    
+    def sync_public(loud=false, wait=nil)
+      container = self.get_container
+      files = self.get_local_public_files
       
       begin_time = Time.new
       files.each do |file|
@@ -27,17 +38,25 @@ module CloudfileAsset
           start = Time.new
           puts "uploading file - #{file}"
         end
+
         object = container.create_object(file.split(@dir_path).last, false)
         object.load_from_filename(file)
+        
         if loud
           now = Time.new
           diff =  now - start
           total = now - begin_time
           puts "done uploading file - #{file} - #{diff} sec, #{total}"
         end
+        
         sleep wait.to_i if not wait.nil?
       end
       puts "Done uploading"
+    end
+    
+    def test
+      container = self.get_container
+      puts (container.objects - self.get_local_public_files)
     end
   end
 end
--- tasks/cloudfile_asset_sync_tasks.rake
+++ tasks/cloudfile_asset_sync_tasks.rake
@@ -1,4 +1,12 @@
 desc "Sync public folder with cloud files"
-task :cf_asset_host => :environment do
-  CloudfileAsset::sync_public(loud=ENV['loud'], wait=ENV['wait'])
+task :cf_asset_host => "cf_asset_host:default"
+
+namespace :cf_asset_host do
+  task :default => :environment do
+    CloudfileAsset::sync_public(loud=ENV['loud'], wait=ENV['wait'])
+  end
+  
+  task :test => :environment do
+    CloudfileAsset::test
+  end
 end
