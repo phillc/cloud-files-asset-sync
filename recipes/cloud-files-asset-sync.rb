4fdac05f9fe0aa00c1dd75628d82bfe859c5fcb3
phillc <pccampbell@patrickdavis.com>
2009-10-20 13:35:40 -0400

    comment for thought

--- recipes/cloud-files-asset-sync.rb
+++ recipes/cloud-files-asset-sync.rb
@@ -3,6 +3,7 @@ namespace :cf_asset_host do
   task :default, :roles => :web, :only => {:asset_host_syncer => true} do
     rake = fetch(:rake, "rake")
     rails_env = fetch(:rails_env, "production")
+    #maybe we should run this locally...
     run "cd #{current_path}; #{rake} RAILS_ENV=#{rails_env} cf_asset_host" do |channel, stream, data|
       puts data
     end
