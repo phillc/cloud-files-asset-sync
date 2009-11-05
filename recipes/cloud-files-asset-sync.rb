5e419110a17d15c6955285df19ef31190adc4caa
phillc <pccampbell@patrickdavis.com>
2009-11-05 16:49:49 -0500

    run that locall instead

--- recipes/cloud-files-asset-sync.rb
+++ recipes/cloud-files-asset-sync.rb
@@ -1,11 +1,6 @@
 namespace :cf_asset_host do
   desc "Syncs the public directory with Rackspace cloud files"
   task :default, :roles => :app, :only => {:asset_host_syncer => true} do
-    rake = fetch(:rake, "rake")
-    rails_env = fetch(:rails_env, "production")
-    #maybe we should run this locally...
-    run "cd #{current_path}; #{rake} RAILS_ENV=#{rails_env} cf_asset_host" do |channel, stream, data|
-      puts data
-    end
+    `rake cf_asset_host`
   end
 end
