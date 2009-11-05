593ab23908c9a78ece331b41f8f202093d84c58a
phillc <pccampbell@patrickdavis.com>
2009-11-05 15:16:05 -0500

    sync assets with app server, not web

--- recipes/cloud-files-asset-sync.rb
+++ recipes/cloud-files-asset-sync.rb
@@ -1,6 +1,6 @@
 namespace :cf_asset_host do
   desc "Syncs the public directory with Rackspace cloud files"
-  task :default, :roles => :web, :only => {:asset_host_syncer => true} do
+  task :default, :roles => :app, :only => {:asset_host_syncer => true} do
     rake = fetch(:rake, "rake")
     rails_env = fetch(:rails_env, "production")
     #maybe we should run this locally...
